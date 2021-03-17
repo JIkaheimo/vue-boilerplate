f4af5c6a3718eb215e594f71284d95696cfc8d1b
JIkaheimo <jaakko.o.ikaheimo@gmail.com>
2021-03-17 11:34:03 +0200

    Remove unnecessary code.

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,5 @@
 <template>
+  <div class="wrapper">asdasd</div>
   <img alt="Vue logo" src="./assets/logo.png" />
   <HelloWorld msg="Hello Vue 3 + Vite asda sdasdasd" />
 </template>
@@ -11,13 +12,13 @@ export default {
 };
 </script>
 
-<style>
-#app {
+<style scoped lang="postcss">
+.wrapper {
+  @apply text-red-50;
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
   margin-top: 60px;
 }
 </style>
